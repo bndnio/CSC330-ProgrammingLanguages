c6611ef49ae9a9fcf9f6afd433bb0724c578acb4
Brendon Earl <bearl@uvic.ca>
2018-11-19 00:10:52 -0800

    Complete section B (extra pieces)

--- hw6/hw6assignment.rb
+++ hw6/hw6assignment.rb
@@ -7,18 +7,61 @@
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here:
 
-  # Your Enhancements here
+  All_My_Pieces = Piece::All_Pieces + [
+    [[[-2,0], [-1,0], [0,0], [1,0], [2,0]],         # extra long
+     [[0,2], [0,1], [0,0], [0,-1], [0,-2]]],
+      rotations([[0,0], [1,0], [0,1]]),               # short L
+      rotations([[0,0], [1,0], [2,0], [0,1], [1,1]])] # short fat L
+
+  # class method to choose the next piece
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
+
+  def size ()
+    @all_rotations[0].length()
+  end
 
 end
 
 class MyBoard < Board
-  # Your Enhancements here:
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
 
+  # gets the next piece
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
+  # gets the information from the current piece about where it is and uses this
+  # to store the piece on the board itself.  Then calls remove_filled.
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    (0..@current_block.size()-1).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
 end
 
 class MyTetris < Tetris
-  # Your Enhancements here:
-
+  # creates a canvas and the board that interacts with it
+  def set_board
+    @canvas = TetrisCanvas.new
+    @board = MyBoard.new(self)
+    @canvas.place(@board.block_size * @board.num_rows + 3,
+                  @board.block_size * @board.num_columns + 6, 24, 80)
+    @board.draw
+  end
 end
-
 
